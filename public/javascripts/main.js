76b275b722d637541b293a2b5c3838be65af4cc9
Briana <372bmswift27@gmail.com>
2015-11-22 15:47:43 -0600

    adding animation to portfolio section

---
+++ public/javascripts/main.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Briana on 11/22/15.
+ */
+$(document).ready(function(){
+
+
+    function animateInfo(divClass, infoID){
+        $(infoID).stop().animate({height: "75%"}).css({color: "white"});
+        $(divClass).mouseleave(function(){
+            $(infoID).stop().animate({height: "0px"}).css({color: "rgba(0,0,0,0)"});
+        })
+    }
+
+    $('.wedding').hover(function(){
+        animateInfo('.wedding','#weddingInfo');
+    })
+
+    $('.tictactoe').hover(function(){
+        animateInfo('.tictactoe', '#tictactoeInfo');
+    })
+
+    $('.calculator').hover(function(){
+        animateInfo('.calculator','#calculatorInfo');
+    })
+
+    $('.quote').hover(function(){
+        animateInfo('.quote','#quotegenInfo');
+    })
+
+    $('.wikipedia').hover(function(){
+        animateInfo('.wikipedia','#wikiInfo');
+    })
+
+    $('.weather').hover(function(){
+        animateInfo('.weather','#weatherInfo');
+    })
+
+    $('.pomodoro').hover(function(){
+        animateInfo('.pomodoro','#pomodoroInfo');
+    })
+
+    $('.twitch').hover(function(){
+        animateInfo('.twitch','#twitchInfo');
+    })
+
+    $('.wedding2').hover(function(){
+        animateInfo('.wedding2','#weddingInfo2');
+    })
+})

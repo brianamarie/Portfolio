d1fb6573fd2e15d9abc74a2ecbb85109529ce9ef
Briana <372bmswift27@gmail.com>
2016-01-23 15:31:32 -0600

    functional markdown

--- public/javascripts/dataVisualZiplines/uncompiled/Markdowns.js
+++ public/javascripts/dataVisualZiplines/uncompiled/Markdowns.js
@@ -1,28 +1,49 @@
-var UserInput = React.createClass({
+var CommentForm = React.createClass({
     getInitialState: function() {
-        return {data: []};
+      return {text: ''};
     },
-    render: function(){
-        return(
-            <p>Hello... </p>
-        );
-    }
+    rawMarkup: function() {
+        var rawMarkup = marked(this.state.text.toString(), {sanitize:true});
+        return { __html: rawMarkup };
+    },
+    handleTextChange: function(e) {
+      this.setState({text: e.target.value});
+    },
+    render: function() {
+       return (
+           <div className="container">
+                <div className="col-xs-6">
+                   <form className="commentForm">
+                        <input
+                            type="text"
+                            placeholder="Say something"
+                            value={this.state.text}
+                            onChange={this.handleTextChange}
+                        />
+                    </form>
+                </div>
+                <div className="col-xs-6">
+                    <div id="markedText">
+                        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+                    </div>
+                </div>
+           </div>
+       )
+   }
 });
 
-
-var HelloWorld = React.createClass({
-    getInitialState: function() {
-        return {data: []};
-    },
+var CommentBox = React.createClass({
     render: function(){
         return(
-            <p>Hello... </p>
+            <div className = "commentBox">
+                <CommentForm />
+            </div>
         );
     }
 });
 
 var showPage = function(){
-        React.render(<HelloWorld data = {[]} />, document.getElementById('markdown'));
+        React.render(<CommentBox />, document.getElementById('markdown'));
 };
 
 showPage();

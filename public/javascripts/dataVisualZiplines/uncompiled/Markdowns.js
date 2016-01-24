3d11f489450773a882d8d58d5837d8f475f5cc43
Briana <372bmswift27@gmail.com>
2016-01-24 10:05:34 -0600

    react github flavored markdown app done but not added to main portfolio page yet

--- public/javascripts/dataVisualZiplines/uncompiled/Markdowns.js
+++ public/javascripts/dataVisualZiplines/uncompiled/Markdowns.js
@@ -1,9 +1,19 @@
-var CommentForm = React.createClass({
+var MarkdownForm = React.createClass({
     getInitialState: function() {
       return {text: ''};
     },
     rawMarkup: function() {
-        var rawMarkup = marked(this.state.text.toString(), {sanitize:true});
+        var rawMarkup = marked(this.state.text.toString(),
+            {
+            gfm: true,
+            tables: true,
+            breaks: false,
+            pedantic: false,
+            sanitize: true,
+            smartLists: true,
+            smartypants: false
+            }
+        );
         return { __html: rawMarkup };
     },
     handleTextChange: function(e) {
@@ -13,17 +23,16 @@ var CommentForm = React.createClass({
        return (
            <div className="container">
                 <div className="col-xs-6">
-                   <form className="commentForm">
-                        <input
-                            type="text"
-                            placeholder="Say something"
+                   <form className="markdownRaw">
+                        <textarea
+                            placeholder="Write your *markup* here!"
                             value={this.state.text}
-                            onChange={this.handleTextChange}
-                        />
+                            onChange={this.handleTextChange}>
+                        </textarea>
                     </form>
                 </div>
                 <div className="col-xs-6">
-                    <div id="markedText">
+                    <div className="markedText">
                         <span dangerouslySetInnerHTML={this.rawMarkup()} />
                     </div>
                 </div>
@@ -32,18 +41,18 @@ var CommentForm = React.createClass({
    }
 });
 
-var CommentBox = React.createClass({
+var MarkdownBoxes = React.createClass({
     render: function(){
         return(
-            <div className = "commentBox">
-                <CommentForm />
+            <div className = "markdownBoxes">
+                <MarkdownForm />
             </div>
         );
     }
 });
 
 var showPage = function(){
-        React.render(<CommentBox />, document.getElementById('markdown'));
+        React.render(<MarkdownBoxes />, document.getElementById('markdown'));
 };
 
 showPage();

518cf020722871be49ccee53d84669d4349cad6c
Briana <372bmswift27@gmail.com>
2016-01-31 11:45:42 -0600

    react functioning as expected for recent leaderboard

--- public/javascripts/dataVisualZiplines/Leaderboard.js
+++ public/javascripts/dataVisualZiplines/Leaderboard.js
@@ -1,16 +1,74 @@
 /**
  * Created by Briana on 1/24/16.
  */
-var topThirtyDays = 'http://fcctop100.herokuapp.com/api/fccusers/top/recent';
-var topEver = 'http://fcctop100.herokuapp.com/api/fccusers/top/alltime.';
+var AllTime = React.createClass({
+    //will return json of alltime
+    render: function() {
+        return (
+            <div className="allTime">
+            Hello,  all time!
+            </div>
+        )
+    }
+});
 
-var Leaders = React.createClass({
+var Recent = React.createClass({
+    render: function() {
+        return (
+            <div className = "recently">
+                <h1> Hello recently. </h1>
+            </div>
+        )
+    }
+});
+
+var Leader = React.createClass({
+    render: function() {
+        return (
+            <div className="leader">
+                <h2 className="leaderUsername">
+                    {this.props.username}
+                </h2>
+                    <img src={this.props.img} />
+                    <h5> Recent points: {this.props.recent} </h5>
+                    <h5> All time points: {this.props.alltime} </h5>
+            </div>
+        );
+    }
+});
+
+var LeaderBox = React.createClass({
+    //box that will display currently selected data
+    render: function() {
+        console.log('leaderbox working');
+        var leaderNodes = this.props.data.map(function(user){
+            return(
+                <Leader
+                    username={user.username}
+                    key={user.id}
+                    img={user.img}
+                    recent={user.recent}
+                    alltime={user.alltime}>
+
+                </Leader>
+           )
+        });
+        return (
+            <div className="leaderBox">
+                {leaderNodes}
+            </div>
+        )
+    }
+});
+
+var LeaderFrame = React.createClass({
+    //would encase everything, including button that would change data displayed
     getInitialState: function() {
         return {data: []};
     },
     componentDidMount: function() {
         $.ajax({
-            url: 'http://fcctop100.herokuapp.com/api/fccusers/top/recent',
+            url: this.props.url,
             dataType: 'json',
             cache: false,
             success: function(data) {
@@ -22,18 +80,24 @@ var Leaders = React.createClass({
         });
     },
     render: function() {
+        console.log(this.state.data);
         return (
             <div className="leaderboard">
-                Hello, world!
-                {this.state}
+                <Recent />
+                <AllTime />
+                <LeaderBox data={this.state.data} />
+                <button className="alltime"> See best of all time! </button>
+                <button className = "recentTime"> See best of recent times! </button>
             </div>
         )
     }
 });
 
 
+
+
 var showPage = function(){
-    React.render(<Leaders />, document.getElementById('mount'));
+    React.render(<LeaderFrame url='http://fcctop100.herokuapp.com/api/fccusers/top/recent' />, document.getElementById('mount'));
 };
 
 showPage();
